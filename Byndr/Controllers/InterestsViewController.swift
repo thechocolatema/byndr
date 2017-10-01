483e04a0e4fc9f41b91c1b1f122debd3443dfbe9
hxk33m <hxk33m@gmail.com>
2017-10-01 16:18:07 -0400

    Add select/remove button

--- Byndr/Controllers/InterestsViewController.swift
+++ Byndr/Controllers/InterestsViewController.swift
@@ -8,36 +8,55 @@
 
 import UIKit
 import Parse
+import IoniconsSwift
 
 class InterestsViewController: UIViewController {
 
-    
-    @IBAction func submitButtonClick(_ sender: UIButton) {
+    //Array for all values
+    var interestArray : [String] = []
+    @objc func submitButtonClick(_ sender: UIButton) {
         let storyBoard: UIStoryboard = UIStoryboard(name: "App", bundle: nil)
         let newViewController = storyBoard.instantiateViewController(withIdentifier: "appstoryboard")
         self.present(newViewController, animated: true, completion: nil)
     }
     @objc func buttonAction(sender: SelectorButton!) {
-        //print(sender.object.objectId!)
         sender.isSelected = !sender.isSelected
         
         //If button selected push item to array
         if sender.isSelected {
-            
-            print(sender.isSelected)
+            if(interestArray.contains(sender.object.objectId!) == false){
+                interestArray.append(sender.object.objectId!)
+            }
+            //print(sender.isSelected)
             //SelectorButton.setTitle("ON", for: .normal)
         }
             
         //If button not selected remove from array
         else {
-            
-            print(sender.isSelected)
+            if let index = interestArray.index(of:sender.object.objectId!) {
+                if(interestArray.contains(sender.object.objectId!) == true){
+                    interestArray.remove(at: index)
+                }                
+            }
+            //print(sender.isSelected)
             //toggleBT.setTitle("OFF", for: .normal)
         }
+        print(interestArray)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //let image = Ionicons.alert.image(35)
+        let image = Ionicons.arrowRightC.image(35, color: UIColor.white)
+        let submitButton = UIButton(type: .custom)
+        submitButton.frame = CGRect(x: 330, y: 656, width: 70, height: 70)
+        submitButton.layer.cornerRadius = 0.5 * submitButton.bounds.size.width
+        submitButton.clipsToBounds = true
+        submitButton.setImage(image, for: .normal)
+        submitButton.backgroundColor = UIColor(red:0.21, green:0.61, blue:0.93, alpha:1.0)
+        submitButton.addTarget(self, action: #selector(submitButtonClick(_:)), for: .touchUpInside)
+        view.addSubview(submitButton)
         
         //Hide back button
         navigationItem.hidesBackButton = true
@@ -46,30 +65,27 @@ class InterestsViewController: UIViewController {
         let query = PFQuery(className: "Interest")
         let buttonPadding: CGFloat = 10
         
-        var buttonX : CGFloat = 0
+        var buttonX : CGFloat = 20
 
         query.findObjectsInBackground {(objects: [PFObject]?, error: Error?) -> Void in
             if error == nil{
                 if let objects = objects {
                     for object in objects{
-         
+                        print(buttonX)
                         //Render a button for each object
                         let button = SelectorButton()
-                        
-                        //Set X position to render each button in a different spot
-                        buttonX = buttonX+90
-                        
+
                         //button.backgroundColor = UIColor.clear
                         button.setTitle(object["name"] as? String, for: .normal)
                         button.object = object
                         button.layer.cornerRadius = 20
-                        button.layer.borderWidth = 3
+                        button.layer.borderWidth = 1
                         
                         //Button title and background for different states
                         button.setTitleColor(UIColor.black, for: .normal)
                         button.isSelected = false
                         button.setBackgroundColor(color: UIColor.clear, forState: .normal)
-                        button.setBackgroundColor(color: UIColor.blue, forState: .selected)
+                        button.setBackgroundColor(color: UIColor(red:0.21, green:0.61, blue:0.93, alpha:1.0), forState: .selected)
                         button.setTitleColor(UIColor.white, for: .selected)
                         
                         button.layer.masksToBounds = true
@@ -78,11 +94,15 @@ class InterestsViewController: UIViewController {
                         let stringsize: CGSize = button.titleLabel!.text!.size(withAttributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 24.0)])
                         
                         //Set button width and height to the width,height of its text
-                        button.frame = CGRect(x:buttonX,y:150,width:stringsize.width+buttonPadding, height:stringsize.height+buttonPadding);
+                        button.frame = CGRect(x:buttonX,y:stringsize.height+buttonPadding+40,width:stringsize.width+buttonPadding, height:stringsize.height+buttonPadding);
                         
+                        //print(button.object.objectId!)
                         //Button click event
                         button.addTarget(self, action: #selector(self.buttonAction(sender:)), for: .touchUpInside)
                         self.view.addSubview(button)
+                        
+                        //Set X position to render each button in a different spot
+                        buttonX = buttonX+90
                     }
                 }
             }else{
